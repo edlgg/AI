d6716922d88acddec7e2a4caa3dce40265b0d518
edlgg <delagarzaguerra@gmail.com>
2019-12-10 09:30:20 +0000

    .

--- NN_Kaggle/setup.sh
+++ NN_Kaggle/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+rm -rf venv
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install pandas numpy scikit-learn tensorflow jupyter ipykernel
-python -m ipykernel install --user --name venv --display-name "My Env"
+python -m ipykernel install --user --name venv --display-name "Custom Env"
